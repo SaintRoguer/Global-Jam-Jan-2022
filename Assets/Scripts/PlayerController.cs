5a56782c8d07b42c3faae50bbc3811e2964dc212
Matias <matiaspippig@gmail.com>
2022-01-21 21:30:08 -0300

    Boludeces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float jumpForce;
+    public float gravityModifier;
+    private bool isOnGround = true;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        isOnGround = true;
     }
 }
