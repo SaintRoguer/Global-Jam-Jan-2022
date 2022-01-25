0c9f2b91320fca8594ad63b13bbf30fe14805b2f
SaintRoguer <anonymousvsworld@gmail.com>
2022-01-25 20:59:37 -0300

    Door controller created
    Missing door object, sprites and hitbox, as well as when the bullet hits the door ask for opposite in DoorController script to use an action.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public CombinationState doorColour = CombinationState.VIOLET;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public bool Opposite( CombinationState bulletColour)
+    {
+        return bulletColour switch
+        {
+            CombinationState.YELLOW => doorColour == CombinationState.VIOLET,
+            CombinationState.BLUE => doorColour == CombinationState.ORANGE,
+            CombinationState.RED => doorColour == CombinationState.GREEN,
+            CombinationState.VIOLET => doorColour == CombinationState.YELLOW,
+            CombinationState.ORANGE => doorColour == CombinationState.BLUE,
+            CombinationState.GREEN => doorColour == CombinationState.RED,
+            _ => false,
+        };
+    }
+
+}
