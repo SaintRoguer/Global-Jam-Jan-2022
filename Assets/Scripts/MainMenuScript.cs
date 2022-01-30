94e96353fbe17b7e350299d19bef1a074d4dc0a1
Matias <matiaspippig@gmail.com>
2022-01-30 15:03:31 -0300

    Intro Sound fix?

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public static AudioClip intro;
+    public static AudioSource audioSrc;
+    public AudioMixer masterMixer, sfxMixer;
     // Start is called before the first frame update
     void Start()
     {
-        SoundManagerScript.PlaySound("intro-voice");
+        //SoundManagerScript.PlaySound("intro-voice");
+        intro = Resources.Load<AudioClip>("Intro Voice");
+
+        SoundManagerScript.Stop();
+        audioSrc = GetComponent<AudioSource>();
+        audioSrc.Stop();
+        audioSrc.PlayOneShot(intro);
+        SoundManagerScript.PlaySound("alive");
     }
 
     // Update is called once per frame
