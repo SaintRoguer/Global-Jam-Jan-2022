a6265f3568463ea75770a859c4523ed3b6396c8f
EliosHERB <elias.herbsommer@gmail.com>
2022-01-30 21:59:04 -0300

    Arreglo de dialogos

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -17,7 +17,7 @@ public class DialogueController : MonoBehaviour
         historia = new List<string>();
         //Add all the text
         historia.Add("You know, this is not an easy task. Being THE DEATH I mean.\n It's a lot of paperwork, and see who goes where and the things like that.\n But luckily they sent you, my new Little Angel!\n Other angels died before, but with the power of my new weapon, the mighty\n CHROMABLAST \n you can collect the colors to open portals among the realms.");
-        historia.Add("Try to collect three souls, they are like little lights with heart pulsation.\n Have in mind each color destroy their opposite and...\n Good luck!");
+        historia.Add("Try to collect three souls, they are like little lights with heart pulsation.\n Keep in mind that each color destroys their oposite and...\n Good luck!");
     }
 
     // Update is called once per frame
